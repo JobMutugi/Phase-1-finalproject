18fffba42aa2dd54fc18ab6101f8c101a344978c
JobMutugi <creativejobu@gmail.com>
2025-06-25 11:15:03 +0300

    add all changes

--- index.js
+++ index.js
@@ -2,20 +2,31 @@ const button = document.querySelector('.btn');
 
 if (button) {
     button.addEventListener('click', () => {
-        window.location.href = 'http://127.0.0.1:5502/products.html';
+        window.scrollTo({ top: 0, behavior: 'smooth' });
     });
 }
+
 
 const home = document.querySelector('.home');
 if (home) {
     home.addEventListener('click', () => {
-        window.location.href = 'http://127.0.0.1:5502/index.html';
+        window.scrollTo({ top: 0, behavior: 'smooth' });
     });
 }
+
 
 const pay = document.querySelector('.pay');
 if (pay) {
     pay.addEventListener('click', () => {
-        window.location.href = 'http://127.0.0.1:5502/payment.html';
+        window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
     });
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  const button = document.querySelector('.btn');
+  const imagesContainer = document.getElementById('images');
+
+  button.addEventListener('click', function () {
+    imagesContainer.classList.add('clear');
+  });
+});
